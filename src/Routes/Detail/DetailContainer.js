3092f61b8be2fe6438fbeaf2d14794c675d2b517
JaejinLee <ppaimar16@gmail.com>
2021-04-15 02:53:12 +0900

    :art: #5.5 Detail Container part One
    #5.5 Detail Container part One

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -9,7 +9,22 @@ export default class extends React.Component{
         loading: true
     };
 
+    async componentDidMount(){
+        const {
+            match: {
+                params: {id}
+            },
+            history: {push}
+        } = this.props;
+        const parseId = parseInt(id);
+        if(isNaN(parseId)){
+            return push("/");//return없이 해주면 사용자를 다른 페이지로 보내고 콘솔로그가 실행된다. 
+        }//왜냐면 자바스크립트가 함수를 끝내지 않았기 떄문이다.
+        //console.log(this.props)
+    }
+
     render() {
+        //console.log(this.props)
         const { result, error, loading } = this.state;
         return (
             <DetailPresenter result={result} error={error} loading={loading}/>

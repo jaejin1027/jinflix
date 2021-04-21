1182d7a809bcc5cf12fde6428a1c211ac9c6b8e7
JaejinLee <ppaimar16@gmail.com>
2021-04-21 23:56:50 +0900

    :art: #6.2 TVPresenter and Loader Components 1
    #6.2 TVPresenter and Loader Components 1

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,8 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types"
 import styled from "styled-components";
+import Section from "Components/Section";
 
-const TVPresenter = ({topRated, popular, airingToday, error, loading}) => null;
+const Container = styled.div`
+  padding:0px 10px;
+`;
+
+const TVPresenter = ({topRated, popular, airingToday, error, loading}) => 
+  loading ? null : (
+  <Container>
+    {topRated && topRated.length > 0 && (
+     <Section title="Top Rated Shows">
+      {topRated.map(show => show.name)}  
+     </Section>
+    )}
+    {popular && popular.length > 0 && (
+     <Section title="Popular Shows">
+      {popular.map(show => show.name)}
+     </Section>
+    )}
+    {airingToday && airingToday.length > 0 && (
+     <Section title="Airing Today">
+      {airingToday.map(show => show.name)}
+     </Section>
+    )}
+  </Container> 
+  ); //topRated가 존재하면, topRated가 0보다클때 component section을 렌더링
 
 TVPresenter.propTypes = {
     topRated:PropTypes.array, 

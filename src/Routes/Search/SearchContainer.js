eee6370e2691107e56463130c21d35cab14d026f
JaejinLee <ppaimar16@gmail.com>
2021-04-22 15:37:32 +0900

    :art: #6.3 SearchPresenter Component
    #6.3 SearchPresenter Component

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -2,67 +2,72 @@ import { moviesApi, tvApi } from "api";
 import React from "react";
 import SearchPresenter from "./SearchPresenter";
 
+export default class extends React.Component {
+  state = {
+    movieResults: null, //누군가 검색 시도 시, 양쪽 영화,티비 결과를 둘다 보여주기 위해
+    tvResults: null,
+    searchTerm: "",
+    error: null,
+    loading: false,
+  };
 
-export default class extends React.Component{
-    state = {
-        movieResults: null, //누군가 검색 시도 시, 양쪽 영화,티비 결과를 둘다 보여주기 위해
-        tvResults: null,
-        searchTerm: "",
-        error: null,
-        loading: false
-    };
-
-    handlSubmit = () => {
-        const {searchTerm} = this.state;
-        if(searchTerm !== ""){
-          this.searchByTerm();
-        }
-    };
-
-    searchByTerm = async () => {
-        const {searchTerm} = this.state;
-        this.setState({
-            loading: true
-        });
-        try {
-            const {data:{results:movieResults}} = await moviesApi.search(searchTerm);
-            const {data:{results:tvResults}} = await tvApi.search(searchTerm);
-            this.setState(
-                {
-                    movieResults,
-                    tvResults
-                }
-            );
-
-        } catch {
-            this.setState(
-                {
-                    error: "Can't find results."
-                }
-            );
-        } finally {
-            this.setState(
-                {
-                    loading: false
-                }
-            );
-
-        }
-
-
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { searchTerm } = this.state;
+    if (searchTerm !== "") {
+      this.searchByTerm();
     }
+  };
 
-    render() {
-        const { movieResults, tvResults, searchTerm, error, loading } = this.state;
-        return (
-            <SearchPresenter 
-              movieResults={movieResults} 
-              tvResults={tvResults} 
-              searchTerm={searchTerm} 
-              error={error} 
-              loading={loading}
-              handlSubmit={this.handlSubmit}
-            />
-        )
-    };  
+  updateTerm = (e) => {
+    const {
+      target: { value },
+    } = e;
+    console.log(value);
+    this.setState({
+      searchTerm: value,
+    });
+  };
+
+  searchByTerm = async () => {
+    const { searchTerm } = this.state;
+    this.setState({
+      loading: true,
+    });
+    try {
+      const {
+        data: { results: movieResults },
+      } = await moviesApi.search(searchTerm);
+      const {
+        data: { results: tvResults },
+      } = await tvApi.search(searchTerm);
+      this.setState({
+        movieResults,
+        tvResults,
+      });
+    } catch {
+      this.setState({
+        error: "Can't find results.",
+      });
+    } finally {
+      this.setState({
+        loading: false,
+      });
+    }
+  };
+
+  render() {
+    const { movieResults, tvResults, searchTerm, error, loading } = this.state;
+    return (
+      <SearchPresenter
+        movieResults={movieResults}
+        tvResults={tvResults}
+        searchTerm={searchTerm}
+        error={error}
+        loading={loading}
+        handleSubmit={this.handleSubmit}
+        updateTerm={this.updateTerm}
+      />
+    );
+  }
 }
